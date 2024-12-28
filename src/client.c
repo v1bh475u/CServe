0c6aadeba65b9731d480005594aeabe74a7b1828
vibhatsu <maulikbarot2915@gmail.com>
2024-12-28 22:44:33 +0530

    add: (client) sample main

---
+++ src/client.c
@@ -0,0 +1,26 @@
+#include "network.h"
+
+int main(int argc, char **argv)
+{
+    int listenfd, connfd;
+    socklen_t clientlen;
+    struct sockaddr_storage clientaddr;
+    char client_hostname[MAXLINE], client_port[MAXLINE];
+
+    if (argc != 2)
+    {
+        fprintf(stderr, "usage: %s <port>\n", argv[0]);
+        exit(0);
+    }
+
+    listenfd = open_listenfd(argv[1]);
+    while (1)
+    {
+        clientlen = sizeof(struct sockaddr_storage);
+        connfd = accept(listenfd, (struct sockaddr *)&clientaddr, &clientlen);
+        getnameinfo((struct sockaddr *)&clientaddr, clientlen, client_hostname, MAXLINE, client_port, MAXLINE, 0);
+        printf("Connected to (%s, %s)\n", client_hostname, client_port);
+        close(connfd);
+    }
+    exit(0);
+}
